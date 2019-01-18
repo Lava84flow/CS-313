1e23aaab68e198861ee49e39df0118ba212ae158
Lava84flow <Lava84flow@users.noreply.github.com>
2019-01-18 00:37:52 -0800

    Finished Core

--- week_02/team_02.js
+++ week_02/team_02.js
@@ -3,3 +3,10 @@
 function changeButton(node) {
     node.innerHTML = 'Clicked!';
 }
+
+function changeColor(choice, node) {
+    color = document.getElementById(choice.id).value;
+    
+    document.getElementById(node.id).style.backgroundColor = color;
+
+}
