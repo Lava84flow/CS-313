d3146cb16dd5ea8134e5db864abee0e51cf4a5d0
Lava84flow <Lava84flow@users.noreply.github.com>
2019-01-19 16:46:26 -0800

    setup php skeleton

---
+++ shared/footer.php
@@ -0,0 +1,7 @@
+<span>
+    <?php echo date('l jS \of F Y h:i:s A P e'); ?> | &copy; Lava84flow
+</span>
+
+<?php
+//echo '<span>' . date('l jS \of F Y h:i:s A e P') . ' | &copy; Lava84flow</span>';
+?>
