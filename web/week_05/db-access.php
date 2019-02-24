785362ae1eb64ecbf96141e96a65cdb1f7ca013e
Lava84flow <Lava84flow@users.noreply.github.com>
2019-02-23 23:29:48 -0800

    Project Initial

---
+++ web/week_05/db-access.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="../shared/main.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	
+	<h1>
+		Blargh
+	</h1>
+	
+</body>
+</html>
